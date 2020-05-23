855f95e182eccd2775aaff917adc29b42b82d136
jaxbulsara <jaxbulsara@gmail.com>
2020-05-22 21:16:31 -0400

    Created package structure.

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+
+setup(
+    name="neogm",
+    packages=["neogm"],
+    version="0.1",
+    license="MIT",
+    description="Object-Graph Mapper for the neo4j bolt driver for Python. Built for Python 3.",
+    author="Jay Bulsara",
+    author_email="jaxbulsara@gmail.com",
+    url="https://github.com/jaxbulsara/neogm",
+    download_url="https://github.com/jaxbulsara/neogm/archive/v_01.tar.gz",
+    keywords=["neo4j", "object-graph-mapper", "ogm", "bolt", "cypher"],
+    install_requires=["neo4j",],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "Topic :: Software Development :: Build Tools",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+    ],
+)
