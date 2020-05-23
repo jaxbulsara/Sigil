7d7f4a347653bb087f595f6497c5d20f197cc8bb
jaxbulsara <jaxbulsara@gmail.com>
2020-05-23 16:58:53 -0400

    Created run method. Refactored session and transaction initialization

--- neogm/cypher.py
+++ neogm/cypher.py
@@ -1,5 +1,6 @@
 from .ogm import GraphObject
 
+import neobolt
 import secrets
 
 CREATE = "CREATE"
@@ -9,22 +10,58 @@ RETURN = "RETURN"
 class Query(object):
     def __init__(self, graph, session=None, transaction=None):
         self._graph = graph
-
-        if transaction:
-            self._session = transaction.session
-            self._transaction = transaction
-
-        elif session:
-            self._session = session
-            self._transaction = self._session.begin_transaction()
-
-        else:
-            self._session = graph.driver.session()
-            self._transaction = self._session.begin_transaction()
-
         self._statement = ""
         self._names = []
         self._return_type = None
+
+        self.initialize(session, transaction)
+
+    def initialize(self, session=None, transaction=None):
+        def _init_existing_transaction():
+            self._session = transaction.session
+            self._transaction = transaction
+            self._close_session_on_run = False
+            self._close_transaction_on_run = False
+
+        def _init_existing_session():
+            self._session = session
+            self._transaction = self._session.begin_transaction()
+            self._close_session_on_run = False
+            self._close_transaction_on_run = True
+
+        def _init_new_session():
+            self._session = self._graph.driver.session()
+            self._transaction = self._session.begin_transaction()
+            self._close_session_on_run = True
+            self._close_transaction_on_run = True
+
+        if transaction:
+            _init_existing_transaction()
+
+        elif session:
+            _init_existing_session()
+
+        else:
+            _init_new_session()
+
+    def run(self, raise_errors=False):
+        try:
+            record = self._transaction.run(self._statement).data()
+
+        except neobolt.exceptions.ClientError as client_error:
+            self._transaction.success = False
+            if raise_errors:
+                raise client_error
+
+        else:
+            self._transaction.success = True
+
+        finally:
+            if self._close_transaction_on_run:
+                self._transaction.close()
+
+            if self._close_session_on_run:
+                self._session.close()
 
     def create(self, graph_object, name=None):
         if not name:
